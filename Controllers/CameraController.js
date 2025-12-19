f4f79b721d6e9de32dfd52e7013c0a656547be80
Malak-Abdelakher <126920530+Malak-Abdelakher@users.noreply.github.com>
2025-12-19 15:50:24 +0200

    Revert to previous version
    Removed commented-out code and unused methods from CameraController.

--- Controllers/CameraController.js
+++ Controllers/CameraController.js
@@ -1,55 +1,3 @@
-// import * as THREE from 'three';
-// import { PlayerController } from './PlayerController.js';
-// import { KeyboardController } from './keyboardController.js';
-// import { MouseController } from './MouseController.js';
-// import { CollisionController } from './collisionController.js';
-
-// export class CameraController {
-//   constructor(camera, scene, walls) {
-//     this.camera = camera;
-//     this.player = new PlayerController(camera);
-//     scene.add(this.player.getObject());
-
-//     this.keyboard = new KeyboardController();
-//     this.mouse = new MouseController(this.player.getObject(), camera);
-
-//     this.collision = new CollisionController(this.player.getObject(), walls);
-//     this.player.getObject().prevPosition = this.player.getObject().position.clone();
-
-//     this.clock = new THREE.Clock();
-//   }
-
-//   update() {
-//     const delta = this.clock.getDelta();
-//     const input = this.keyboard.getDirection();
-//     const move = this.player.speed * delta;
-
-//     this.player.getObject().prevPosition = this.player.getObject().position.clone();
-
-//     // --- Movement ---
-//     if (input.forward) this.player.moveForward(move);
-//     if (input.backward) this.player.moveForward(-move);
-//     if (input.left) this.player.moveRight(-move);
-//     if (input.right) this.player.moveRight(move);
-
-//     // --- Jump ---
-//     if (this.keyboard.keys['Space']) {
-//       this.player.jump();
-//     }
-
-//     // --- Gravity & vertical movement ---
-//     this.player.update(delta);
-
-//     // --- Collision check ---
-//     this.collision.update();
-//   }
-
-//   getPlayer() {
-//     return this.player.getObject();
-//   }
-// }
-// CameraController.js
-// CameraController.js
 import * as THREE from 'three';
 import { PlayerController } from './PlayerController.js';
 import { KeyboardController } from './keyboardController.js';
@@ -59,8 +7,8 @@ import { CollisionController } from './collisionController.js';
 export class CameraController {
   constructor(camera, scene, walls) {
     this.camera = camera;
-    this.scene = scene;
     this.player = new PlayerController(camera);
+  
     scene.add(this.player.getObject());
 
     this.keyboard = new KeyboardController();
@@ -70,21 +18,6 @@ export class CameraController {
     this.player.getObject().prevPosition = this.player.getObject().position.clone();
 
     this.clock = new THREE.Clock();
-  }
-
-  /**
-   * Register portal meshes with collision system
-   * Call this from main.js after portals are placed
-   */
-  registerPortalForCollision(portalMesh) {
-    this.collision.registerPortalMesh(portalMesh);
-  }
-
-  /**
-   * Clear portals from collision
-   */
-  clearPortalsFromCollision() {
-    this.collision.clearPortals();
   }
 
   update() {
@@ -114,32 +47,5 @@ export class CameraController {
 
   getPlayer() {
     return this.player.getObject();
-  }
-
-  /**
-   * Get player velocity for teleportation
-   */
-  getVelocity() {
-    if (this.player && this.player.velocity) {
-      return this.player.velocity.clone();
-    }
-    return new THREE.Vector3();
-  }
-
-  /**
-   * Set player velocity (used after teleportation)
-   */
-  setVelocity(velocity) {
-    if (this.player && this.player.velocity) {
-      this.player.velocity.copy(velocity);
-      console.log('📊 Velocity updated after teleport:', velocity);
-    }
-  }
-
-  /**
-   * Get the actual player controller for advanced operations
-   */
-  getPlayerController() {
-    return this.player;
   }
 }
